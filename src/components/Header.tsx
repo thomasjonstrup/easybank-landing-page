bbcdfa30308d6de2e07b14cd807ccca0485b8bf2
Thomas Jonstrup <thomas_jonstrup@hotmail.com>
2021-06-22 18:05:13 +0200

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header({children}) {
+	return (
+		<header className="header">
+			<div className="container">{children}</div>
+		</header>
+	);
+}
